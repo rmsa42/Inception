bdffcc2a2f943314b2a436802412888b664f0b03
rmsa42 <rui.soares.almeida@outlook.pt>
2024-09-13 20:02:00 +0100

    Connected the mariadb to wordpress. Failed to load the url

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -x
 
-mariadb-install-db
-service mariadb start
-mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS test2; CREATE USER IF NOT EXISTS rumachad@localhost IDENTIFIED BY 'inception';"
-mariadbd
+chown -R mysql:mysql /var/lib/mysql
+mysqld_safe &
+sleep 5
+mysql -u root -e "
+	CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+	CREATE USER IF NOT EXISTS ${DB_USER}@'%' IDENTIFIED BY '${DB_PASSWORD}';
+	GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@'%';
+"
+mysqladmin -u root shutdown
+exec mysqld
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -2,12 +2,14 @@
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp-cli
+mv wp-cli.phar /usr/local/bin/wp
 
 cd /var/www/html
 chmod -R 777 /var/www/html
 su www-data -s /bin/bash -c "
 	cd /var/www/html
-	wp-cli core download
+	wp core download
+	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST}
+	wp core install --url=test.com --title=Hello --admin_user=adminTest --admin_password=inception --admin_email=test@example.com
 "
 exec php-fpm7.4 -F
