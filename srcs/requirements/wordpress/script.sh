88565864c57b8ed18ef57bd35e2fb387bf6d66d5
rmsa42 <rui.soares.almeida@outlook.pt>
2024-09-16 15:23:29 +0100

    Opened wordpress site with all containers

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -5,11 +5,11 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 cd /var/www/html
-chmod -R 777 /var/www/html
+chown -R www-data:www-data /var/www/html
 su www-data -s /bin/bash -c "
 	cd /var/www/html
 	wp core download
 	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST}
-	wp core install --url=test.com --title=Hello --admin_user=adminTest --admin_password=inception --admin_email=test@example.com
+	wp core install --url=localhost --title=Hello --admin_user=adminTest --admin_password=inception --admin_email=test@example.com
 "
 exec php-fpm7.4 -F
