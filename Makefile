58796bde9539b30e9307accf9f603366b6ba31a3
rmsa42 <rui.soares.almeida@outlook.pt>
2024-09-18 17:16:26 +0100

    Added ssl keys as secrets, restart on-failure and a new user command in wordpress

--- Makefile
+++ Makefile
@@ -2,11 +2,8 @@ DOCKER_COMPOSE_PATH = ./srcs/docker-compose.yml
 
 build:
 	@docker compose -f ${DOCKER_COMPOSE_PATH} up -d --build
-stop:
-	@docker compose -f ${DOCKER_COMPOSE_PATH} stop
-kill:
-	@docker compose -f ${DOCKER_COMPOSE_PATH} kill
 down:
 	@docker compose -f ${DOCKER_COMPOSE_PATH} down
 clean:
 	@docker system prune -a
+re: down build
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -4,12 +4,11 @@ set -x
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-
 cd /var/www/html
 chown -R www-data:www-data /var/www
 su www-data -s /bin/bash -c "
-	cd /var/www/html
-	wp core download
+wp core download
+if [ ! -f "./wp-config.php" ]; then
 	wp config create \
 		--dbname=${DB_NAME} \
 		--dbuser=${DB_USER} \
@@ -17,8 +16,14 @@ su www-data -s /bin/bash -c "
 		--dbhost=${DB_HOST}
 	wp core install --url=${WP_URL} \
 		--title='${WP_TITLE}' \
-		--admin_user=${WP_ADMIN_USER} -\
-		-admin_password=$(head -n 1 ${WP_ADMIN_PASS}) \
+		--admin_user=${WP_ADMIN_USER} \
+		--admin_password=$(head -n 1 ${WP_ADMIN_PASS}) \
 		--admin_email=${WP_ADMIN_EMAIL}
+	wp user create \
+		UserTest \
+		UserTest@example.com \
+		--role=contributor \
+		--user_pass=test
+fi
 "
 exec php-fpm7.4 -F
